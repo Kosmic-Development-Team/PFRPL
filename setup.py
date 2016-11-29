474bb19ab087db1d842490efe446c2cb2eabeb59
KosmicCo <cruzbarnum@gmail.com>
2016-11-29 00:03:34 -0800

    Made into a library.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='PFRPL',
     version='1.0',
     packages=['flow'],
-    url='',
+    url='https://github.com/Kosmic-Development-Team/PFRPL',
     license='',
     author='Kosmic Development Team',
-    author_email='',
+    author_email='cruzbarnum@gmail.com',
     description='Functional Reactive Programming from Weirdbob95\'s Forge on Github'
 )
